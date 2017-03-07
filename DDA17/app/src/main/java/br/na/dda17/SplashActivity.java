e1f4d54be413dcdbbebda62c7ab105d95314a9ea
lanabia <lanabeatriz.mesquita@gmail.com>
2017-03-06 22:16:16 -0300

    Ícone launcher e correção

--- DDA17/app/src/main/java/br/na/dda17/SplashActivity.java
+++ DDA17/app/src/main/java/br/na/dda17/SplashActivity.java
@@ -3,38 +3,31 @@ package br.na.dda17;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 
-import java.util.Timer;
-import java.util.TimerTask;
 
-/**
- * Created by 349059 on 20/06/16.
- */
-public class SplashActivity extends Activity {
+public class SplashActivity extends AppCompatActivity {
 
-    private static final int duracao = 2000;
+    private final int SPLASH_DISPLAY_LENGTH = 2000;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
         setContentView(R.layout.activity_splash);
-        super.onCreate(savedInstanceState);
-    }
 
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        TimerTask t = new TimerTask() {
-            // Esse método será executado sempre que o timer acabar e chamar a  activity principal
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent in = new Intent().setClass(SplashActivity.this, MainActivity.class);
-                startActivity(in);
-                finish();
-
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
             }
+        }, SPLASH_DISPLAY_LENGTH);
 
-        };
 
     }
 }
