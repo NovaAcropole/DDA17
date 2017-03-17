08945215a393e81ce743a33d564f4d952fa22291
suelenmc <suelenmc@gmail.com>
2017-03-17 03:58:10 -0300

    Ajeitando dialog do Egg

--- DDA17/app/src/main/java/br/na/dda17/activity/ProgramacaoActivity.java
+++ DDA17/app/src/main/java/br/na/dda17/activity/ProgramacaoActivity.java
@@ -1,11 +1,17 @@
 package br.na.dda17.activity;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
 import com.github.florent37.materialviewpager.MaterialViewPager;
 import com.github.florent37.materialviewpager.header.HeaderDesign;
@@ -18,6 +24,7 @@ import butterknife.ButterKnife;
 public class ProgramacaoActivity extends AppCompatActivity {
 
     MaterialViewPager mViewPager;
+    final Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,6 +69,32 @@ public class ProgramacaoActivity extends AppCompatActivity {
                         ContextCompat.getDrawable(ProgramacaoActivity.this, R.drawable.cine));
             }
         });
+
+        // EEGG
+        final View logo = findViewById(R.id.logo_white);
+
+        if (logo != null) {
+            logo.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // custom dialog
+                    final Dialog dialog = new Dialog(context);
+                    dialog.setContentView(R.layout.dialog_egg);
+                    final Button dialogButton = (Button) dialog.findViewById(R.id.dialog_button_egg);
+
+                    dialogButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.egg_link)));
+                            v.getContext().startActivity(i);
+
+                            dialog.dismiss();
+                        }
+                    });
+                    dialog.show();
+                }
+            });
+        }
 
     }
 
--- DDA17/app/src/main/java/br/na/dda17/activity/SobreActivity.java
+++ DDA17/app/src/main/java/br/na/dda17/activity/SobreActivity.java
@@ -1,5 +1,9 @@
 package br.na.dda17.activity;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.content.ContextCompat;
@@ -7,6 +11,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 import android.app.Activity;
 import android.app.FragmentManager;
@@ -19,13 +24,13 @@ import com.github.florent37.materialviewpager.header.HeaderDesign;
 
 import br.na.dda17.R;
 import br.na.dda17.Utils;
-import br.na.dda17.fragment.EggDialogFragment;
 import br.na.dda17.fragment.RecyclerViewFragment;
 import butterknife.ButterKnife;
 
 public class SobreActivity extends AppCompatActivity {
 
     MaterialViewPager mViewPager;
+    final Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -74,22 +79,26 @@ public class SobreActivity extends AppCompatActivity {
 
         // EEGG
         final View logo = findViewById(R.id.logo_white);
+
         if (logo != null) {
             logo.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                   /** mViewPager.notifyHeaderChanged();
-                    Toast.makeText(getApplicationContext(), getResources().getString(R.string.eegg),
-                            Toast.LENGTH_LONG).show();**/
+                    // custom dialog
+                    final Dialog dialog = new Dialog(context);
+                    dialog.setContentView(R.layout.dialog_egg);
+                    final Button dialogButton = (Button) dialog.findViewById(R.id.dialog_button_egg);
 
-                    FragmentManager manager = getFragmentManager();
-                    android.app.Fragment frag = manager.findFragmentByTag("fragment_edit_name");
-                    if (frag != null) {
-                        manager.beginTransaction().remove(frag).commit();
-                    }
-                            EggDialogFragment alertDialogFragment = new EggDialogFragment();
-                            alertDialogFragment.show(manager, "fragment_edit_name");
+                    dialogButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.egg_link)));
+                            v.getContext().startActivity(i);
 
+                            dialog.dismiss();
+                        }
+                    });
+                    dialog.show();
                 }
             });
         }
